e3b6988747c5edf781f5c73601c59e9de3684861
Ranjith kumar <ranjit4072@gmail.com>
2020-05-19 19:33:52 +0530

    Moved todoFooter as a new component

---
+++ src/components/TodoFooter.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "./TodoFooter.css";
+
+const TodoFilter = props => {
+  const { todoFilter, handleFilter, filter } = props;
+  return (
+    <div
+      onClick={() => handleFilter(`${filter}`)}
+      className={todoFilter === `${filter}` ? "active" : ""}
+    >
+      {filter}
+    </div>
+  );
+};
+
+const TodoFooter = props => {
+  const { todoFilter, handleFilter } = props;
+  return (
+    <div className="todo-container-footer">
+      <div className="todo-items-msg">1 items left</div>
+      <div className="todo-status-container">
+        {["All", "Active", "Completed"].map((filter, index) => {
+          return (
+            <TodoFilter
+              key={index}
+              filter={filter}
+              todoFilter={todoFilter}
+              handleFilter={handleFilter}
+            />
+          );
+        })}
+      </div>
+      <div className="todo-clear-container">Clear completed</div>
+    </div>
+  );
+};
+
+export default TodoFooter;
